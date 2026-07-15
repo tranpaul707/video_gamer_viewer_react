a28c26bcf86925d10553ca1cf56d9c1acefde1a3
Paul Tran <paultran@Pauls-MacBook-Air.local>
2026-07-14 22:46:32 -0400

    Added a Carousel component, redone css for main page, adjusted css for Navigation component

--- games_viewer/src/App.tsx
+++ games_viewer/src/App.tsx
@@ -1,10 +1,37 @@
 import Genre from "./components/Genre/Genre";
 import Navigation from "./components/Navigation/Navigation";
+import Carousel from "./components/Carousel/Carousel";
+import "./App.css";
+
 function App() {
   return (
     <>
-      <Navigation />
-      <Genre />
+      <main>
+        <header>
+          <nav>
+            <Navigation />
+          </nav>
+        </header>
+        <div className="page-body">
+          <aside>
+            <Genre />
+          </aside>
+
+          <div className="content">
+            <section>
+              <Carousel />
+            </section>
+
+            <section>
+              <Carousel />
+            </section>
+
+            <section>
+              <Carousel />
+            </section>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
