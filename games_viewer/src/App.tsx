8751115a66ac94daee5b2202f255b93a09563060
Paul Tran <paultran@Pauls-MacBook-Air.local>
2026-07-17 23:06:26 -0400

    Added .env, revised .gitignore, added API for Steamspy and fetching behavior, resolved CORs issue through vite.config.ts

--- games_viewer/src/App.tsx
+++ games_viewer/src/App.tsx
@@ -1,9 +1,19 @@
 import Genre from "./components/Genre/Genre";
 import Navigation from "./components/Navigation/Navigation";
 import Carousel from "./components/Carousel/Carousel";
+import { fetchSteamAPI, type SteamSpyGame } from "./api/steam";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [games, setGames] = useState<SteamSpyGame[]>([]);
+
+  useEffect(() => {
+    fetchSteamAPI().then((data) => {
+      if (data) setGames(data);
+    });
+  }, []);
+
   return (
     <>
       <main>
@@ -19,15 +29,11 @@ function App() {
 
           <div className="content">
             <section>
-              <Carousel />
-            </section>
-
-            <section>
-              <Carousel />
-            </section>
-
-            <section>
-              <Carousel />
+              <ul>
+                {games.map((game) => (
+                  <li key={game.appid}>{game.name}</li>
+                ))}
+              </ul>
             </section>
           </div>
         </div>
--- games_viewer/vite.config.ts
+++ games_viewer/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    proxy:{
+      '/steamspy': {
+        target: 'https://steamspy.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/steamspy/, ""),
+      },
+    },
+  },
 })
